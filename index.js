c71d0cdd4937961fa9fc9bf69fcde3cebc716490
roygitstenhub <108938077+roygitstenhub@users.noreply.github.com>
2022-11-29 18:31:36 +0530

    mytest

---
+++ index.js
@@ -0,0 +1,19 @@
+ let arr=["joshwa","Roysten","John","Sajitha","Jonson","sudishma","steven","jack","Shreya"]
+
+ for(let i=0;i<=arr.length;i++)
+ {
+    // console.log(arr[i]);
+    if(arr[i].charAt(0)=='J')
+    {
+        console.log("Goodbye"+" "+arr[i]);
+    }
+    else if(arr[i].charAt(0)=='j')
+    {
+        console.log("Goodbye"+" "+arr[i]);
+
+    }
+    else{
+       console.log("Hello"+" "+arr[i])
+    }
+ }
+
